0a5c536cad4bda96ddbb3b2a1efb31a366f5b13a
karthikraja <karthikrajasenthivel@gmail.com>
2024-05-30 16:14:33 +0530

    Update CricketScoreboard.c

--- CricketScoreboard.c
+++ CricketScoreboard.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-
-// Structure to represent a player's scorecard
 struct Player {
     char name[20];
     int runs;
@@ -9,15 +7,9 @@ struct Player {
     int sixes;
     float strike_rate;
 };
-
 int main() {
-    // Array to hold scorecards of two players
     struct Player players[2];
  char team_name[50];
-
-    // Input team name
-    
-    // Input scorecard details for each player
     for (int i = 0; i < 2; i++) {
         printf("Enter details for Player %d:\n", i + 1);
         printf("Name: ");
@@ -30,20 +22,14 @@ int main() {
         scanf("%d", &players[i].fours);
         printf("6s: ");
         scanf("%d", &players[i].sixes);
-        
-        // Calculate strike rate
         players[i].strike_rate = (float)(players[i].runs) / players[i].balls * 100.0;
     }
 printf("Team name: ");
     scanf("%s",team_name);
-    // Print scorecard header
     printf("\nPlayer\tRuns\tBalls\t4s\t6s\tS.R\n");
-
-    // Print scorecard details for each player
     for (int i = 0; i < 2; i++) {
         printf("%s\t%d\t%d\t%d\t%d\t%.2f\n", players[i].name, players[i].runs, players[i].balls, 
                players[i].fours, players[i].sixes, players[i].strike_rate);
     }
-
     return 0;
 }
