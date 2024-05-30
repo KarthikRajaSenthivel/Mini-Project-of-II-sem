81ea6b097e97c720cab341286e0cd684ce650f9c
karthikraja <karthikrajasenthivel@gmail.com>
2024-05-30 16:15:56 +0530

    Update FLAMES.c

--- FLAMES.c
+++ FLAMES.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 #include <string.h>
-
-// Function to calculate FLAMES result
 char calculateFLAMES(char name1[], char name2[]) {
     int i, j, count1 = 0, count2 = 0;
     char result;
-    
-    // Convert names to lowercase
     for (i = 0; name1[i]; i++) {
         if (name1[i] >= 'A' && name1[i] <= 'Z')
             name1[i] += 32; // Convert uppercase to lowercase
@@ -15,13 +11,11 @@ char calculateFLAMES(char name1[], char name2[]) {
         if (name2[i] >= 'A' && name2[i] <= 'Z')
             name2[i] += 32; // Convert uppercase to lowercase
     }
-    
     // Count occurrence of each character in name1
     for (i = 0; name1[i]; i++) {
         if (name1[i] != ' ') // Exclude spaces
             count1++;
     }
-    
     // Count occurrence of each character in name2
     for (i = 0; name2[i]; i++) {
         if (name2[i] != ' ') // Exclude spaces
@@ -45,17 +39,11 @@ char calculateFLAMES(char name1[], char name2[]) {
 int main() {
     char name1[100], name2[100], result;
     int i;
-    
-    // Input names
     printf("Enter name 1: ");
     gets(name1);
     printf("Enter name 2: ");
     gets(name2);
-    
-    // Calculate FLAMES result
     result = calculateFLAMES(name1, name2);
-    
-    // Print result
     switch (result) {
         case 'F':
             printf("Friendship\n");
@@ -79,6 +67,5 @@ int main() {
             printf("Error\n");
             break;
     }
-    
     return 0;
 }
